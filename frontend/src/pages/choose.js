4b931d9f667cf6e709f4527b026c07d632c3eee1
Vankayalakoutilya7 <123583033+Vankayalakoutilya7@users.noreply.github.com>
2024-04-16 10:55:53 +0530

    choose.js

--- frontend/src/pages/choose.js
+++ frontend/src/pages/choose.js
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
-// import '../css/choose.css'
+import '../css/choose.css'
 
 function ChoosePage() {
   const [selectedValue, setSelectedValue] = useState(null);
 
   const handleRadioButtonChange = (value) => {
     setSelectedValue(value);
-    // Navigate based on selected value
     if (value === 0) {
-      // Navigate to client login page
-      // You may use react-router-dom for navigation
-      // Example: history.push('/login/client');
     } else if (value === 1) {
       // Navigate to technician login page
       // You may use react-router-dom for navigation
@@ -27,7 +23,7 @@ function ChoosePage() {
         <div className="col">
           <CustomRadioButton
             label="Client"
-            image="assets/client.jpg" // Replace with the actual path to the client image
+            image="assets/client.jpg" 
             value={0}
             checked={selectedValue === 0}
             onChange={handleRadioButtonChange}
@@ -36,7 +32,7 @@ function ChoosePage() {
         <div className="col">
           <CustomRadioButton
             label="Technician"
-            image="assets/technician.jpg" // Replace with the actual path to the technician image
+            image="assets/technician.jpg" 
             value={1}
             checked={selectedValue === 1}
             onChange={handleRadioButtonChange}
